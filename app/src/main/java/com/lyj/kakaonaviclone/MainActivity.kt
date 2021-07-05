6a672c4e151427a28630cd41acf0ae50b37d3a33
iyeongjun <banziha104@gmail.com>
2021-07-05 18:33:33 +0900

    1. Custom App Bar 기반 제작

--- app/src/main/java/com/lyj/kakaonaviclone/MainActivity.kt
+++ app/src/main/java/com/lyj/kakaonaviclone/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lyj.kakaonaviclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.lyj.api.BaseApi
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -15,9 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("lyjjj","module = ${myInt.myInf()}")
-        var api = BaseApi();
-
-        Log.d("lyjjj","module2 = ${api.test()}")
+        var api = BaseApi()
+//        var layout : CollapsingToolbarLayout  = findViewById(R.id.collapsingToolbar)
+//        layout.isTitleEnabled = true
     }
 }
