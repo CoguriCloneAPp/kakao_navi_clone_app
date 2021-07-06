c5ed4d3d4499de0c5af542dfa375a3ef86414e0c
iyeongjun <banziha104@gmail.com>
2021-07-06 18:02:53 +0900

    1. Custom App Bar 기반 제작

--- app/src/main/java/com/lyj/kakaonaviclone/MainActivity.kt
+++ app/src/main/java/com/lyj/kakaonaviclone/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.lyj.kakaonaviclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.lyj.api.BaseApi
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var myInt: MyInt
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var api = BaseApi()
-//        var layout : CollapsingToolbarLayout  = findViewById(R.id.collapsingToolbar)
-//        layout.isTitleEnabled = true
+        var aname = 10
     }
 }
---
+++ app/src/main/java/com/lyj/kakaonaviclone/keystore/Key.kt
@@ -0,0 +1,7 @@
+package com.lyj.kakaonaviclone.keystore
+
+class Key {
+    companion object{
+        const val KAKAO_KEY = "506791764bf6b1c2b8d3915d71a9d8f7"
+    }
+}
